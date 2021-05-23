b637f48b0675de1cb9d4274b91bf07d9cf9e4477
Rafael Márquez <ing17romc@gmail.com>
2021-05-23 11:10:28 -0500

    respaldo

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
+const request = require("request");
 const app = express();
-const puerto = 8008;
+const puerto = process.env.PUERTO;
+const api_ml_busqueda = process.env.API_MERCADO_LIBRE_BUSQUEDA;
 
 app.use(cors());
 app.use(express.json());
@@ -13,6 +15,55 @@ app.get('/', function (req, res) {
     message: 'Servicio corriendo',
   });
 });
+
+app.get('/api/items/', function (req, res) {
+  console.log(req.query);
+  console.log(api_ml_busqueda + req.query.q)
+  request(api_ml_busqueda + req.query.q, (err, response, body) => {
+    if (!err) {
+      const respuesta_api = JSON.parse(body);
+      // console.log(respuesta_api)
+
+      const resultados = [];
+
+      respuesta_api.results.forEach(element => resultados.push(
+        {
+          "id": element.id,
+          "title": element.title,
+          "price": {
+            "currency": element.currency_id,
+            "amount": element.available_quantity,
+            "decimals": formatterPeso.format(element.price),
+          },
+          picture: element.thumbnail,
+          "condition": element.condition,
+          "free_shipping": element.shipping.free_shipping,
+          state_name: element.address.state_name,
+        }
+
+      ));
+
+      const respuesta = {
+        author: {
+          name: 'nombre',
+          lastname: 'apellido',
+        },
+        categories: ['categoria 1', 'categoria 1', 'categoria 1'],
+
+        items: resultados,
+      };
+
+      res.json(respuesta);
+    }
+  })
+});
+
+const formatterPeso = new Intl.NumberFormat('es-AR', {
+  style: 'currency',
+  currency: 'ARS',
+  minimumFractionDigits: 0,
+  useGrouping: true,
+})
 
 app.listen(puerto, function () {
   console.log(`servidor web escuchando por el puerto ${puerto}`);
